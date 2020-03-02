ddd5042ce03a5ea211260864c3e3897b179effcf
yihuang <yi.codeplayer@gmail.com>
2020-03-02 12:49:31 +0800

    Make TrustedState serializable

--- tendermint-lite/src/store.rs
+++ tendermint-lite/src/store.rs
@@ -9,7 +9,7 @@ pub type State = TrustedState<block::signed_header::SignedHeader, block::header:
 #[derive(Default)]
 pub struct MemStore {
     height: Height,
-    store: HashMap<Height, State>,
+    store: HashMap<Height, String>,
 }
 
 impl MemStore {
@@ -25,17 +25,18 @@ impl MemStore {
     pub fn add(&mut self, trusted: State) -> Result<(), Error> {
         let height = trusted.last_header().header().height();
         self.height = height;
-        self.store.insert(height, trusted);
+        self.store
+            .insert(height, serde_json::to_string(&trusted).unwrap());
         Ok(())
     }
 
-    pub fn get(&self, h: Height) -> Result<&State, Error> {
+    pub fn get(&self, h: Height) -> Result<State, Error> {
         let mut height = h;
         if h == 0 {
             height = self.height
         }
         match self.store.get(&height) {
-            Some(state) => Ok(state),
+            Some(s) => Ok(serde_json::from_str(&s).unwrap()),
             None => Err(Kind::RequestFailed
                 .context(format!("could not load height {} from store", height))
                 .into()),
